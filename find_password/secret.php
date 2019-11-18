ab81d869cf512b39ca4d78630d1838b45d3b6509
Imreplay <imreplay@imreplay.com>
2019-11-18 11:38:54 +0900

    Create secret.php

---
+++ find_password/secret.php
@@ -0,0 +1,19 @@
+<?php
+$message = "This is my password!";
+$u_id = "my_id";
+$u_pw = "hello_my_password!";
+if(isset($_POST['pw'])){
+  if($_POST['pw']==$u_pw){
+    echo "Yes!!!";
+  }
+  else{
+    echo "No....";
+  }
+}
+?>
+
+</br>Can you find my id/pw?</br>
+
+<form action="" method="post">
+password <input type="text" name="pw"> <input type="submit">
+</form>
